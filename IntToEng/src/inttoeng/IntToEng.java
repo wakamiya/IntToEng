0bb8783974b30168a7d590cd19336e8a6026cb44
wakamiya <g12960wm@s30452.tsuda.ac.jp>
2014-05-09 19:22:56 +0900

    ０の処理にも対応しました。もう少し簡略化したいところです。。。

--- IntToEng/src/inttoeng/IntToEng.java
+++ IntToEng/src/inttoeng/IntToEng.java
@@ -18,11 +18,12 @@ public class IntToEng {
     	String Eng2[] = {"twenty","thirty","fourty","fifty","sixty","seventy","eightty","ninety"};
     
     	int x=0,y=0,z=0;
-    	if(0<=n && n<10) {return Eng[n]+"";}
+    	if(n==0) return "zero";
+    	if(0<n && n<10) {return Eng[n]+"";}
     	else if(9<n && 20>n) {return Eng1[n-10]+"";}
     	else {
     			while(1000<n && n<10000) {
-    				n = n-10000;
+    				n = n-1000;
     				x++;
     			}
     			while(99<n && n<1000){
@@ -33,11 +34,13 @@ public class IntToEng {
     			n = n-10;
     			z++;
     		}
-    		//if(x==0 && y==0 && n==0) return Eng2[z-2];
     		if(x==0 && y==0) return Eng2[z-2]+"-"+Eng[n]+"";
-    		//else if(x==0 && n ==0) return Eng[y] + " hundred "+Eng2[z-2];
+    		else if(x==0 && z==0) return Eng[y]+" hundred "+Eng[n];
+    		else if(y==0 && z==0) return Eng[x]+" thousand "+Eng[n];
     		else if(x==0) return Eng[y]+" hundred "+Eng2[z-2]+"-"+Eng[n];
-    		else  return Eng[x]+" thousand "+ Eng[y]+" hendred and "+Eng2[x-2]+"-"+Eng[n];
+    		else if(y==0) return Eng[x]+" thousand "+ Eng2[z-2]+"-"+Eng[n];
+    		else if(z==0) return Eng[x]+" thousand "+ Eng[y]+" hundred "+Eng[n];
+    		else  return Eng[x]+" thousand "+ Eng[y]+" hundred and "+Eng2[z-2]+"-"+Eng[n];
     		
     	}
     	
